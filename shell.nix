2d67fd9655657b59c34a9cc61d752f3b2794df9c
rcmlz <19784049+rcmlz@users.noreply.github.com>
2026-01-31 14:35:29 +0100

    Add NixOS

---
+++ shell.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import <nixpkgs> {} }:
+ pkgs.mkShell {
+    packages = [
+      pkgs.rakudo
+      pkgs.zef
+      
+      pkgs.git
+      pkgs.curl
+      
+      pkgs.readline
+      pkgs.cacert
+      pkgs.zlib
+      pkgs.openssl        
+      pkgs.zeromq
+    ];
+
+    # Avoid this error: Cannot locate native library 'libreadline.so.7': libreadline.so.7: cannot open shared object file: No such file or directory
+    # or: Cannot locate native library 'libssl.so': libssl.so: cannot open shared object file: No such file or directory
+    # etc.
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ 
+      pkgs.readline
+      pkgs.cacert
+      pkgs.zlib
+      pkgs.openssl
+      pkgs.zeromq
+    ];
+
+    # Set zef environment variables
+    ZEF_FETCH_DEGREE = 4;
+    ZEF_TEST_DEGREE = 4;
+
+    shellHook = ''
+      echo including $HOME/.raku/bin in PATH
+      export PATH="$HOME/.raku/bin:$PATH"
+
+      echo installing dependenciees of current module
+      zef install --debug --deps-only .
+
+      echo installing current module
+      zef install --debug .
+    '';    
+}
